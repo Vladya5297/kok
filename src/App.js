df4281dc82793be28cf622f7fe833a01995b6a85
Vladya5297 <123dobro@mail.ru>
2021-02-19 20:20:34 +0300

    feat: barrier selection added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import classes from './App.module.css'
 
-const Barrier = ({ number, remove, P, setP, withH, H }) => {
+const Barrier = ({ number, remove, P, setP, withH, H, checked, setChecked }) => {
   const [um, setUm] = useState(0)
   const [fm, setFm] = useState(0)
-  
-  const recalculateP = (um, fm) => {
+
+  useEffect(() => {
     if (um === 0 || fm === 0 || (withH && H === 0)) {
       setP(NaN)
     } else {
@@ -14,14 +14,18 @@ const Barrier = ({ number, remove, P, setP, withH, H }) => {
         : (1 / um) / (1 / um + 1 / fm)
       setP(result)
     }
-  }
-
-  useEffect(() => {
-    recalculateP(um, fm)
-  }, [H, withH])
+  }, [H, withH, um, fm])
 
   return (
     <div className={classes.barrier}>
+      <input
+        type='checkbox'
+        onChange={({ target }) => {
+          setChecked(target.checked)
+        }}
+        disabled={number === 1}
+        checked={checked}
+      />
       <span>{`Преграда ${number}`}</span>
       <span>um</span>
       <input
@@ -31,7 +35,6 @@ const Barrier = ({ number, remove, P, setP, withH, H }) => {
         onChange={({ target }) => {
           const value = parseFloat(target.value)
           setUm(value)
-          recalculateP(value, fm)
         }}/>
       <span>fm</span>
       <input
@@ -41,27 +44,37 @@ const Barrier = ({ number, remove, P, setP, withH, H }) => {
         onChange={({ target }) => {
           const value = parseFloat(target.value)
           setFm(value)
-          recalculateP(um, value)
         }}/>
       <span>{`Pнсд = ${P || 'Деление на ноль'}`}</span>
+      <span>{`Pзащ = ${1 - P || 'Деление на ноль'}`}</span>
       {number !== 1 && <button onClick={remove}>Удалить</button>}
     </div>
   )
 }
 
 const App = () => {
-  const [barriers, setBarriers] = useState([{ id: 1 }])
+  const [barriers, setBarriers] = useState([{ id: 1, checked: true }])
   const [withH, setWithH] = useState(false)
   const [H, setH] = useState(0)
 
   const deleteBarrier = (id) => {
-    setBarriers(barriers.filter(barrier => barrier.id !== id))
+    setBarriers(barriers => barriers.filter(barrier => barrier.id !== id))
   }
 
   const setP = (id, P) => {
-    setBarriers(barriers.map(barrier => {
+    setBarriers(barriers => barriers.map(barrier => {
       if (barrier.id === id) {
-        return { id, P }
+        return Object.assign({}, barrier, { P })
+      } else {
+        return barrier
+      }
+    }))
+  }
+
+  const setChecked = (id, checked) => {
+    setBarriers(barriers => barriers.map(barrier => {
+      if (barrier.id === id) {
+        return Object.assign({}, barrier, { checked })
       } else {
         return barrier
       }
@@ -69,6 +82,7 @@ const App = () => {
   }
 
   const total = 1 - barriers.reduce((accumulator, barrier) => {
+    if (!barrier.checked) return accumulator
     accumulator = accumulator * barrier.P
     return accumulator
   }, 1)
@@ -92,12 +106,14 @@ const App = () => {
           </>
         }
       </span>
-      {barriers.map(({ id, P }, index) => (
+      {barriers.map(({ id, P, checked }, index) => (
         <Barrier
           key={id}
           number={index + 1}
           remove={() => { deleteBarrier(id) }}
           setP={(P) => setP(id, P)}
+          setChecked={(checked) => setChecked(id, checked)}
+          checked={checked}
           P={P}
           withH={withH}
           H={H}
@@ -107,7 +123,7 @@ const App = () => {
         type='button'
         value='Добавить преграду'
         onClick={() => {
-          setBarriers([...barriers, { id: barriers.length + 1 }])
+          setBarriers([...barriers, { id: barriers.length + 1, checked: true }])
         }}
       />
       <div className={classes.total}>{`Pзащ = ${total || 'Невозможно вычислить'}`}</div>
